dc7e75dd316586e759de67df032247d58f3be826
Mert Balasar <79214655+mertbalasar@users.noreply.github.com>
2023-01-03 19:27:49 +0300

    created worker

---
+++ TradeRofit.TokenWorker/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using TradeRofit.DAL.Repository;
+using TradeRofit.Entities.Models;
+
+namespace TradeRofit.TokenWorker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                }).UseSerilog();
+    }
+}
--- TradeRofit.TokenWorker/Worker.cs
+++ TradeRofit.TokenWorker/Worker.cs
@@ -1,8 +1,32 @@
-﻿using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TradeRofit.DAL.Repository;
+using TradeRofit.Entities.Models;
 
 namespace TradeRofit.TokenWorker
 {
-    public class Class1
+    public class Worker : BackgroundService
     {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger,
+            IServiceProvider serviceProvider)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
     }
 }
